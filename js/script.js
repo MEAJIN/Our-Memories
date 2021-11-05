36ad54a2512a41ef08a79c57f397e462153fbf72
MEAJIN <kimo3o98@gmail.com>
2021-11-05 12:32:48 +0900

    1105 로그인 폼 닫기 버튼 구현 및 반응형 수정

--- js/script.js
+++ js/script.js
@@ -1,47 +1,63 @@
-// search
+// search, 검색 버튼 클릭 후 발생 이벤트
 const searchForm = document.querySelector('.search-form');
 
 document.querySelector('#search').onclick = () => {
   searchForm.classList.toggle('active');
   loginForm.classList.remove('active');
   navbar.classList.remove('active');
+  loginFormCloseAf.classList.remove('active');
 }
 
-//desktop-login
-const desktopLoginForm = document.querySelector('.login-form');
-
-document.querySelector('#sign-in').onclick = () => {
-  loginForm.classList.toggle('active');
-  searchForm.classList.remove('active');
-  navbar.classList.remove('active');
-}
-
-//mobile-login
+//login-on, 로그인 폼 on
 const loginForm = document.querySelector('.login-form');
 
 document.querySelector('.sign-in').onclick = () => {
   loginForm.classList.toggle('active');
+  loginFormCloseAf.classList.toggle('active');
   searchForm.classList.remove('active');
   navbar.classList.remove('active');
 }
 
-//menu-btn-on
+//login-close-btn-on,  로그인 폼 닫기 버튼 활성
+const loginFormCloseBtn = document.querySelector('.login-form-close-btn');
+
+document.querySelector('#sign-in').onclick = () => {
+  loginFormCloseBtn.classList.toggle('active');
+  loginForm.classList.toggle('active');
+  searchForm.classList.remove('active');
+  navbar.classList.remove('active');
+}
+
+//login-form-close, 모든 로그인 폼 닫기 버튼 클릭 > 로그인 폼 off
+const loginFormCloseAf = document.querySelector('.login-form-close-af');
+
+document.querySelector('#form-close').onclick = () => {
+  loginFormCloseAf.classList.remove('active');
+  loginFormCloseBtn.classList.remove('active');
+  loginForm.classList.remove('active');
+  navbar.classList.remove('active');
+}
+
+//menu-btn-on, 메뉴 버튼 클릭 후 발생 이벤트
 const navbar = document.querySelector('.navbar');
 
 document.querySelector('#menu').onclick = () => {
   navbar.classList.toggle('active');
   searchForm.classList.remove('active');
   loginForm.classList.remove('active');
+  loginFormCloseBtn.classList.remove('active');
 }
 
-//menu-btn-off
+//menu-btn-off, 메뉴 닫기 버튼 클릭 후 발생 이벤트
 const close = document.querySelector('.close');
 
-document.querySelector('#menu-box-close').onclick = () => {
+document.querySelector('#box-close').onclick = () => {
   close.classList.toggle('active');
   navbar.classList.remove('active');
   searchForm.classList.remove('active');
   loginForm.classList.remove('active');
+  loginFormCloseBtn.classList.remove('active');
+  loginFormCloseAf.classList.remove('active');
 }
 
 //remove
@@ -50,4 +66,6 @@ window.click = () => {
   loginForm.classList.remove('active');
   navbar.classList.remove('active');
   close.classList.remove('active');
+  loginFormCloseBtn.classList.remove('active');
+  loginFormCloseAf.classList.remove('active');
 }
